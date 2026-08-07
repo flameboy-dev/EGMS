2a285a6ad9fd116108e24ae385191402416a9daf
flameboy <shuvogit9932github@gmail.com>
2026-08-07 15:06:04 +0530

    chore: fix the position of the image of program cards

--- frontend/src/components/custom/ProgramCard.jsx
+++ frontend/src/components/custom/ProgramCard.jsx
@@ -18,8 +18,12 @@ function ProgramCard({
         className={`relative flex min-h-[331px] w-full max-w-[387px] flex-col justify-between rounded-[45px] border-2 border-[#191A23] px-[35px] py-[40px] shadow-[0_5px_0_0_#191A23] transition-all duration-300 hover:-translate-y-1 ${bgColor}`}
       >
         <div>
-          {/* Top Row: Image (97.82x97.82), Title & Top-Right Badge */}
-          <div className="relative flex items-center gap-4">
+          {/* Top Row: Title on Left, Image on Right & Top-Right Icon Badge */}
+          <div className="relative flex items-center justify-between gap-4">
+            <h3 className="mt-2 pr-6 font-fredoka text-lg font-semibold leading-snug text-[#000000] sm:text-xl">
+              {title}
+            </h3>
+
             {image && (
               <ImageWithSkeleton
                 src={image}
@@ -28,13 +32,10 @@ function ProgramCard({
                 className="h-[97.82px] w-[97.82px] object-contain"
               />
             )}
-            <h3 className="mt-2 pr-8 font-fredoka text-lg font-semibold leading-snug text-[#000000] sm:text-xl">
-              {title}
-            </h3>
 
             {/* Top Right Black Icon Badge */}
             {IconComponent && (
-              <div className="absolute top-0 right-0 flex h-8 w-8 items-center justify-center rounded-full bg-[#000000] text-white">
+              <div className="absolute top-0 right-0 flex h-8 w-8 items-center justify-center rounded-full bg-[#000000] text-white z-10">
                 <IconComponent className="h-4 w-4" />
               </div>
             )}
@@ -52,7 +53,7 @@ function ProgramCard({
     );
   }
 
-  // Program Card Layout
+  // Program Card Layout (Nursery, LKG, UKG, STD-I to STD-IV)
   const programCardClasses = `relative flex h-[331px] w-full max-w-[387px] flex-col justify-between overflow-hidden rounded-[32px] border-2 border-[#191A23] p-8 shadow-[0_5px_0_0_#191A23] transition-all duration-300 hover:-translate-y-1 ${bgColor}`;
 
   const ProgramContent = (
@@ -68,13 +69,16 @@ function ProgramCard({
         )}
       </div>
 
+      {/* Right Side Illustration Image */}
       {image && (
-        <ImageWithSkeleton
-          src={image}
-          alt={`${title} Illustration`}
-          wrapperClassName="pointer-events-none absolute bottom-3 right-3 z-0 max-h-[175px] max-w-[210px]"
-          className="h-auto max-h-[175px] max-w-[210px] object-contain"
-        />
+        <div className="pointer-events-none absolute bottom-3 right-3 z-0 max-h-[175px] max-w-[210px] flex items-end justify-end">
+          <ImageWithSkeleton
+            src={image}
+            alt={`${title} Illustration`}
+            wrapperClassName="max-h-[175px] max-w-[210px]"
+            className="h-auto max-h-[175px] max-w-[210px] object-contain"
+          />
+        </div>
       )}
     </>
   );
